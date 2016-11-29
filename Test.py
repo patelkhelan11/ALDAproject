1a54fc2e7f52ff6ec78e8a40c1c579da93f03469
patelkhelan11 <patelkhelan11@gmail.com>
2016-11-29 08:59:36 -0500

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-from sklearn import preprocessing
-le = preprocessing.LabelEncoder()
-le.fit(['marital','nonmarital','single'])
-a=list(le.transform(['marital','nonmarital','single','marital','nonmarital','single']))
-print(a)
-le.fit(['play','not play'])
-b=list(le.transform(['play','not play','play','not play','play','not play']))
-print(b)
